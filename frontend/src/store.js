a45b9adc38f6fda47c81580e4741340589fb746f
juansechaux <juansechaux@hotmail.com>
2024-02-29 14:44:31 -0500

    create front-end user authentication, user(login, reducer and action), user(login, screen and funtionality), user in Navbar and Logout, user Register(reducer and action), update profile endpoint, profile screen and get user details and finally update the user profile

--- frontend/src/store.js
+++ frontend/src/store.js
@@ -3,18 +3,27 @@ import { thunk } from 'redux-thunk'
 import { composeWithDevTools } from 'redux-devtools-extension'
 import { productListReducers, productDetailsReducers } from './reducers/productReducers'
 import { cartReducer } from './reducers/cartReducers'
+import { userLoginReducers, userRegisterReducers, userDetailsReducers, userUpdateProfileReducers } from './reducers/userReducer'
 
 const reducer = combineReducers({
     productList: productListReducers,
     productDetails: productDetailsReducers,
     cart: cartReducer,
+    userLogin: userLoginReducers,
+    userRegister: userRegisterReducers,
+    userDetails: userDetailsReducers,
+    userUpdateProfile: userUpdateProfileReducers,
 })
 
 const cartItemsFromStorage = localStorage.getItem('cartItems') ?
     JSON.parse(localStorage.getItem('cartItems')) : []
 
+const userInfoFromStorage = localStorage.getItem('userInfo') ?
+    JSON.parse(localStorage.getItem('userInfo')) : null
+
 const initialState = {
-    cart: { cartItems: cartItemsFromStorage }
+    cart: { cartItems: cartItemsFromStorage },
+    userLogin: { userInfo: userInfoFromStorage }
 };
 
 const middleware = [thunk];
