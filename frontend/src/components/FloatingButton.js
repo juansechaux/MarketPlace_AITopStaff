cc03a037a35a17d0c580d7feb8857718c8ad42ca
Glozano <lozsuperbia2014@gmail.com>
2024-03-01 16:00:47 -0500

    create the file FloatingButton.js in the file componenrts of frontend and updated the file App.js with the component FloatingButton

---
+++ frontend/src/components/FloatingButton.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import { Link } from 'react-router-dom';
+import './FloatingButton.js'; // Estilos CSS para el botón flotante
+
+function FloatingButton() {
+  return (
+    <div className="position-fixed bottom-0 end-0 p-3">
+      <Link to="https://lidarit.com/" className="btn btn-secondary rounded-pill">Create your AITopStaff</Link>
+    </div>
+  );
+}
+
+export default FloatingButton;
